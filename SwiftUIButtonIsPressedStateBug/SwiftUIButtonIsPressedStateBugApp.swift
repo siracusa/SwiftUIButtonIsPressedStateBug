b3b9e7bab40b824d540642a5c780c43a4ef3a4cb
John Siracusa <siracusa@gmail.com>
2021-11-10 10:01:56 -0500

    Initial Commit

---
+++ SwiftUIButtonIsPressedStateBug/SwiftUIButtonIsPressedStateBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonIsPressedStateBugApp.swift
+//  SwiftUIButtonIsPressedStateBug
+//
+//  Created by John Siracusa on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonIsPressedStateBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
